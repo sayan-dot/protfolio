90c7bc7b99b925c617d24845e9cb98b73e1804c3
sayan-dot <shuvradeep.96@gmail.com>
2024-11-01 11:29:38 +0530

    projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -16,7 +16,7 @@ const portfolioProjects = [
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: "https://houseofmango.in/",
     image: darkSaasLandingPage,
   },
   {
